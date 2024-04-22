a9cf57779660a748bda7ff1f9566ab1c26fa1d2a
Peyton Politewicz <peyton.james.pz@gmail.com>
2024-04-22 16:28:06 -0700

    updated tests and sorting functions

--- project1_tests.py
+++ project1_tests.py
@@ -2,10 +2,38 @@ import sorting
 import random
 import time
 import pandas as pd
+import sys
+import math
 
-names = ["MergeSort", "QuickSort", "InsertionSort", "ShellSort1", "ShellSort2", "BucketSort", "RadixSort", "BinaryInsertionSort"]
+sys.setrecursionlimit(2000000000 ) #Let Quicksort cook
+print("Recursion limit is:", sys.getrecursionlimit())
+#generate almost-sorted swapmap function
+def genSwapmap(n):
+    numpairs = math.trunc(2*math.log(n))
+    i = []
+    j = []
+    for iter in range(1,numpairs+1):
+        idx1 = random.randint(0,n-1)
+        idx2 = random.randint(0,n-1)
+        if idx1 not in i and idx1 not in j and idx2 not in i and idx2 not in j:
+            i.append(idx1)
+            j.append(idx2)
+    return i, j
 
-sorter = [None] * 8
+''' Unused; == comparison is equivalent here, simpler, less prone to looping weirdness.
+# Verification that sorting works
+def verify(data, test_data, index):
+        if test_data == data:
+            print(names[index], "Sorted!")
+        else:
+            print(names[index], "Successfully Sorted!")
+'''
+
+### Initialization
+
+names = ["MergeSort", "QuickSort", "InsertionSort", "ShellSort1", "ShellSort2", "BucketSort", "RadixSort", "CustomSort1", "MoMSort"]
+
+sorter = [None] * 9
 sorter[0] = sorting.MergeSort()
 sorter[1] = sorting.QuickSort()
 sorter[2] = sorting.InsertionSort()
@@ -14,63 +42,85 @@ sorter[4] = sorting.ShellSort([1000,100,10,1])
 sorter[5] = sorting.BucketSort(1000)
 sorter[6] = sorting.RadixSort()
 sorter[7] = sorting.BinaryInsertionSort()
+sorter[8] = sorting.MoMSort()
 
-# Verification Intializations
-DATA_SIZE = 500
-NUM_EXP = 5
+SIZES = [500, 1000, 2000, 4000, 8000] # Data sizes to iterate across
+#NUM_EXP = 5 Redundant if looping using seed array.
+SEEDS = random.sample(range(1,1000),4)
+SEEDS.insert(0, 260)
+
 data = []
-
-# Verification that sorting works
-def verify(data, test_data, index):
-    for i in range(DATA_SIZE):
-        if test_data[i] != data[i]:
-            print(names[index], "Incorrect Sorting!")
-            exit
-    print(names[index], "Successfully Sorted!")
-
-for i in range(DATA_SIZE):
-    data.append(random.randint(0,1000))
-    
-data_save = data.copy()
-test_data = sorted(data)
-
-for i in range(len(sorter)):
-    data = data_save.copy()
-    sorter[i].sort(data)
-    verify(data,test_data,i)
-
-
-# Testing Initializations
-DATA = [1000, 2000, 4000, 8000, 16000]
-NUM_RUNS = 5
 uniform_results = []
-almost_sorted_results = []
+almost_results = []
+swap1 = []
+swap2 = []
 
-# Uniformly Distributed Testing and Results
-for size in DATA:
-    for i in range(NUM_RUNS):
-        random.seed(random.randint(0,500))
-        #print()
-        #print("DATA SIZE:", size)
+### Uniform loop beginning
+for SIZE in SIZES:
+    
+    for SEED in SEEDS: #conduct a trial for every seed and algorithm at the current data size.
+        random.seed(SEED)
+        
+        #create random data.
         data = []
-        for k in range(size):
+        for i in range(SIZE):
             data.append(random.randint(0,1000))
+        
+        data_save = data.copy()
+        test_data = sorted(data)
 
         for j in range(len(sorter)):
-            data_copy = data.copy()
+            data = data_save.copy()  # Reset data to its original unsorted state before sorting.
             start_time = time.perf_counter()
-            sorter[j].sort(data_copy)
+            sorter[j].sort(data)
             end_time = time.perf_counter()
+
+            if test_data != data:
+                print(names[j], SIZE, SEED,  "FAILED")
+            
             sorter[j].time = end_time - start_time
-            #print(names[j], sorter[j].time)
-            uniform_results.append({'Algorithm': names[j], 'Data Size': size, 'Runtime': sorter[j].time, 'Theoretical Big-O': 0})
+            uniform_results.append({'Algorithm': names[j], 'Seed': SEED, 'Data Size': SIZE, 'Runtime': sorter[j].time, 'Theoretical Big-O': 0})
+
+### Almost-sorted loop beginning
+for SIZE in SIZES:
+    
+    for SEED in SEEDS: #conduct a trial for every seed and algorithm at the current data size.
+        random.seed(SEED)
+
+        swap1, swap2 = genSwapmap(SIZE) #Create consistent swapmap for the seed
+
+        #create random data.
+        data = []
+        for i in range(SIZE):
+            data.append(random.randint(0,1000))
+        
+        data_save = data.copy()
+        test_data = sorted(data)
+        data = sorted(data_save) #create sorted dataset
+
+        for i, j in zip(swap1, swap2):
+            data[i], data[j] = data[j], data[i]
+
+        for j in range(len(sorter)):
+            data = data_save.copy()  # Reset data to its original unsorted state before sorting.
+            start_time = time.perf_counter()
+            sorter[j].sort(data)
+            end_time = time.perf_counter()
+
+            if test_data != data:
+                print(names[j], SIZE, SEED,  "FAILED")
+            
+            sorter[j].time = end_time - start_time
+            almost_results.append({'Algorithm': names[j], 'Seed': SEED, 'Data Size': SIZE, 'Runtime': sorter[j].time, 'Theoretical Big-O': 0})
+
 
 uniform_results_df = pd.DataFrame(uniform_results)
-uniform_results_df.to_csv('uniform_results.csv', index=False)
+almost_results_df = pd.DataFrame(almost_results)
+print(uniform_results_df)
+print(almost_results_df)
+#uniform_results_df.to_csv('uniform_results.csv', index=False)
 
 # Almost Sorted Testing and Results
-
-
 
 #almost_sorted_results_df = pd.DataFrame(almost_sorted_results)
 #almost_sorted_results_df.to_csv('almost_sorted_results.csv', index=False)
--- sorting.py
+++ sorting.py
@@ -2,253 +2,319 @@
 # each file that uses a graph should import it from this file.
 import random as rand
 from collections.abc import Iterable
+import sys
+
+sys.setrecursionlimit(2000000) #Let Quicksort cook
+
 
 class MergeSort:
-	def __init__(self):
-		self.time = 0
+    def __init__(self):
+        self.time = 0
 
-	def sort(self, data):
-		'''
-		Sort the list data using MergeSort
-	 	@param list data to be sorted
-		'''
-		self.sortHelper(data, 0, len(data))
-		return data
+    def sort(self, data):
+        '''
+        Sort the list data using MergeSort
+         @param list data to be sorted
+        '''
+        self.sortHelper(data, 0, len(data))
+        return data
 
-	def sortHelper(self, data, low, high):
-		'''
-		MergeSort helper method.  Sorts data >= start and < end
-		
-		@param list data to be sorted
-		@param low start of the data to be sorted
-		@param high end of the data to be sorted (exclusive)
-		'''
-		
-		if high - low > 1:
-			mid = low + (high-low)//2
+    def sortHelper(self, data, low, high):
+        '''
+        MergeSort helper method.  Sorts data >= start and < end
+        
+        @param list data to be sorted
+        @param low start of the data to be sorted
+        @param high end of the data to be sorted (exclusive)
+        '''
+        
+        if high - low > 1:
+            mid = low + (high-low)//2
 
-			self.sortHelper(data, low, mid)	
-			self.sortHelper(data, mid, high)
-			self.merge(data, low, mid, high)
+            self.sortHelper(data, low, mid)	
+            self.sortHelper(data, mid, high)
+            self.merge(data, low, mid, high)
 
-	def merge(self, data, low, mid, high):
-		'''
-		Merge data >= low and < high into sorted data.  Data >= low and < mid are in sorted order.
-		Data >= mid and < high are also in sorted order
-		
-		@param data the partially sorted data
-		@param low bottom index of the data to be merged
-		@param mid midpoint of the data to be merged
-		@param high end of the data to be merged (exclusive)
-		
-		Note: the merged data must be in the same data array that
-		      was passed as a parameter.
-		'''
-		temp = []
-		i = low
-		j = mid
+    def merge(self, data, low, mid, high):
+        '''
+        Merge data >= low and < high into sorted data.  Data >= low and < mid are in sorted order.
+        Data >= mid and < high are also in sorted order
+        
+        @param data the partially sorted data
+        @param low bottom index of the data to be merged
+        @param mid midpoint of the data to be merged
+        @param high end of the data to be merged (exclusive)
+        
+        Note: the merged data must be in the same data array that
+              was passed as a parameter.
+        '''
+        temp = []
+        i = low
+        j = mid
 
-		while (i < mid and j < high):
-			if data[i] <= data[j]:
-				temp.append(data[i])
-				i += 1
-			else:
-				temp.append(data[j])
-				j += 1
+        while (i < mid and j < high):
+            if data[i] <= data[j]:
+                temp.append(data[i])
+                i += 1
+            else:
+                temp.append(data[j])
+                j += 1
 
-		while (i < mid):
-			temp.append(data[i])
-			i += 1
-			
-		while (j < high):
-			temp.append(data[j])
-			j += 1
+        while (i < mid):
+            temp.append(data[i])
+            i += 1
+            
+        while (j < high):
+            temp.append(data[j])
+            j += 1
 
-		for num in range(len(temp)):
-			data[low + num] = temp[num]
+        for num in range(len(temp)):
+            data[low + num] = temp[num]
 
 
 class QuickSort:
-	def __init__(self):
-		self.time = 0
+    def __init__(self):
+        self.time = 0
 
-	def swap(self, data, index1, index2):
-		temp = data[index1]
-		data[index1] = data[index2]
-		data[index2] = temp
+    def swap(self, data, index1, index2):
+        temp = data[index1]
+        data[index1] = data[index2]
+        data[index2] = temp
 
-	def sort(self, data): # O(n log n) expected, O(n^2) worst depending on pivot
-		'''
-		Sort the list data using QuickSort
-	 	@param list data to be sorted
-		'''
-		self.sortHelper(data, 0, len(data) - 1)
-		return data
+    def sort(self, data): # O(n log n) expected, O(n^2) worst depending on pivot
+        '''
+        Sort the list data using QuickSort
+         @param list data to be sorted
+        '''
+        self.sortHelper(data, 0, len(data) - 1)
+        return data
 
-	def sortHelper(self, data, low, high):
-		'''
- 		Helper method for Quicksort.  Sorts data so that data[lo .. j-1] <= data[j] <= data[j+1 .. hi]
-	 	
-	 	@param data data to be sorted
-	 	@param low start of the data to be sorted (inclusive)
-	 	@param high end of the data to be sorted (exclusive)
-		'''
-		if high <= low:
-			return
-		
-		pivot = self.partition(data, low, high)
+    def sortHelper(self, data, low, high):
+        '''
+         Helper method for Quicksort.  Sorts data so that data[lo .. j-1] <= data[j] <= data[j+1 .. hi]
+         
+         @param data data to be sorted
+         @param low start of the data to be sorted (inclusive)
+         @param high end of the data to be sorted (exclusive)
+        '''
+        if high <= low:
+            return
+        
+        pivot = self.partition(data, low, high)
 
-		self.sortHelper(data, low, pivot - 1)	
-		self.sortHelper(data, pivot + 1, high)
+        self.sortHelper(data, low, pivot - 1)	
+        self.sortHelper(data, pivot + 1, high)
 
-	def partition(self, data, low, high):
-		i = low - 1
-		pivot = data[high]
-		for j in range(low, high):
-			if data[j] <= pivot:
-				i += 1
-				self.swap(data, i, j)
-		i += 1
-		self.swap(data,i,high)
-		return i
+    def partition(self, data, low, high):
+        i = low - 1
+        pivot = data[high]
+        for j in range(low, high):
+            if data[j] <= pivot:
+                i += 1
+                self.swap(data, i, j)
+        i += 1
+        self.swap(data,i,high)
+        return i
 
 
 class InsertionSort:
-	def __init__(self):
-		self.time = 0
+    def __init__(self):
+        self.time = 0
 
-	def swap(self, data, index1, index2):
-		temp = data[index1]
-		data[index1] = data[index2]
-		data[index2] = temp
+    def swap(self, data, index1, index2):
+        temp = data[index1]
+        data[index1] = data[index2]
+        data[index2] = temp
 
-	def sort(self, data): # O(n^2) expected, but O(n) possible if pre-sorted
-		'''
-		Sort the list data using InsertionSort
-	 	@param list data to be sorted
-		'''
-		for i in range(1, len(data)):
-			current = data[i]
-			j = i - 1
-			while (0 <= j and current < data[j]):
-				data[j + 1] = data[j] # use this instead of "self.swap(data, j, j + 1)" faster
-				j -=1
-			data[j + 1] = current
-		return data
+    def sort(self, data): # O(n^2) expected, but O(n) possible if pre-sorted
+        '''
+        Sort the list data using InsertionSort
+         @param list data to be sorted
+        '''
+        for i in range(1, len(data)):
+            current = data[i]
+            j = i - 1
+            while (0 <= j and current < data[j]):
+                data[j + 1] = data[j] # use this instead of "self.swap(data, j, j + 1)" faster
+                j -=1
+            data[j + 1] = current
+        return data
 
 
 class ShellSort:
-	def __init__(self, gap_list):
-		self.time = 0
-		self.gap_list = gap_list
+    def __init__(self, gap_list):
+        self.time = 0
+        self.gap_list = gap_list
 
-	def swap(self, data, index1, index2):
-		temp = data[index1]
-		data[index1] = data[index2]
-		data[index2] = temp
+    def swap(self, data, index1, index2):
+        temp = data[index1]
+        data[index1] = data[index2]
+        data[index2] = temp
 
-	def sort(self, data): # O(n log n) expected, worst case O(n^2)
-		'''
-		Sort the list data using ShellSort
-	 	@param list data to be sorted
-		Gap sequence is stored in self.gap_list upon construction
-		'''
-		for gap in self.gap_list:
-				for i in range(gap, len(data)):
-					current = data[i]
-					j = i
-					while (gap <= j and current < data[j - gap]):
-						self.swap(data, j, j - gap)
-						j -= gap
-					data[j] = current
-		return data
+    def sort(self, data): # O(n log n) expected, worst case O(n^2)
+        '''
+        Sort the list data using ShellSort
+         @param list data to be sorted
+        Gap sequence is stored in self.gap_list upon construction
+        '''
+        for gap in self.gap_list:
+                for i in range(gap, len(data)):
+                    current = data[i]
+                    j = i
+                    while (gap <= j and current < data[j - gap]):
+                        self.swap(data, j, j - gap)
+                        j -= gap
+                    data[j] = current
+        return data
 
 
 class BucketSort:
-	def __init__(self, range):
-		self.time = 0
-		self.range = range + 1
-		self.table = [None] * self.range
-		self.max = 0
+    def __init__(self, range):
+        self.time = 0
+        self.range = range + 1
+        self.table = [None] * self.range
+        self.max = 0
 
-	def insert(self, elem):
-		if self.table[elem] is None:
-			self.table[elem] = [elem]
-		else:
-			self.table[elem].append(elem)
+    def insert(self, elem):
+        if self.table[elem] is None:
+            self.table[elem] = [elem]
+        else:
+            self.table[elem].append(elem)
 
-	def sort(self, data): # O(n + N) where N is the range of values
-		'''
-		Sort the list data using BucketSort
-	 	@param list data to be sorted
-		bucket table is self.table
-		'''
-		for elem in data:
-			self.insert(elem)
+    def sort(self, data): # O(n + N) where N is the range of values
+        '''
+        Sort the list data using BucketSort
+         @param list data to be sorted
+        bucket table is self.table
+        '''
+        self.table = [None] * self.range
+        for elem in data:
+            self.insert(elem)
 
-		link_list = []
-		for bucket in self.table:
-			if bucket is not None:
-				link_list.extend(bucket)
-		data[:] = link_list
-		return data
+        link_list = []
+        for bucket in self.table:
+            if bucket is not None:
+                link_list.extend(bucket)
+        data[:] = link_list
+        return data
 
 
 class RadixSort:
-	def __init__(self):
-		self.time = 0
-		self.table = [None]*10
-		self.max = 0
-		self.digits = 0
+    def __init__(self):
+        self.time = 0
+        self.table = [None]*10
+        self.max = 0
+        self.digits = 0
 
-	def insert(self, elem, iter):
-		digit = ((elem // (10 ** iter)) % 10)
+    def insert(self, elem, iter):
+        digit = ((elem // (10 ** iter)) % 10)
 
-		if self.table[digit] == None:
-			self.table[digit] = [elem]
-		else:
-			self.table[digit].append(elem)
+        if self.table[digit] == None:
+            self.table[digit] = [elem]
+        else:
+            self.table[digit].append(elem)
 
-	def sort(self, data): # O((n+m)*d) = O(n*d) expected where d is maximum # of digits and m is range of values
-		'''
-		Sort the list data using RadixSort
-	 	@param list data to be sorted
-		bucket table is self.table with 10 entries
-		'''
-		self.max = max(data)
-		self.digits = len(str(self.max))
+    def sort(self, data): # O((n+m)*d) = O(n*d) expected where d is maximum # of digits and m is range of values
+        '''
+        Sort the list data using RadixSort
+         @param list data to be sorted
+        bucket table is self.table with 10 entries
+        '''
+        self.max = max(data)
+        self.digits = len(str(self.max))
 
-		for i in range(self.digits):
-			for elem in data:
-				self.insert(elem, i)
+        for i in range(self.digits):
+            for elem in data:
+                self.insert(elem, i)
 
-			link_list = []
-			for bucket in self.table:
-				if bucket is not None:
-					link_list.extend(bucket)
-			
-			data[:] = link_list
-			self.table = [None] * 10
-		return data
+            link_list = []
+            for bucket in self.table:
+                if bucket is not None:
+                    link_list.extend(bucket)
+            
+            data[:] = link_list
+            self.table = [None] * 10
+        return data
 
 
 class BinaryInsertionSort: # Insertion Sort Improvement with Binary Search(Connor)
-	def __init__(self,):
-		self.time = 0
+    def __init__(self,):
+        self.time = 0
 
-	def binary_search(self, data, value, start, end): # O(log n)
-		while start < end:
-			mid = (start + end) // 2
-			if data[mid] < value:
-				start = mid + 1
-			else:
-				end = mid
-		return start
-	
-	def sort(self, data): # O(n) * O(log n) = O(n log n) in best case; O(n^2) worst case
-		for i in range(1, len(data)): # O(n)
-			current = data[i]
-			j = self.binary_search(data, current, 0, i) # O(log n)
-			data[j + 1:i + 1] = data[j:i]
-			data[j] = current
-		return data
+    def binary_search(self, data, value, start, end): # O(log n)
+        while start < end:
+            mid = (start + end) // 2
+            if data[mid] < value:
+                start = mid + 1
+            else:
+                end = mid
+        return start
+    
+    def sort(self, data): # O(n) * O(log n) = O(n log n) in best case; O(n^2) worst case
+        for i in range(1, len(data)): # O(n)
+            current = data[i]
+            j = self.binary_search(data, current, 0, i) # O(log n)
+            data[j + 1:i + 1] = data[j:i]
+            data[j] = current
+        return data
+    
+class MoMSort:
+    def __init__(self):
+        self.time = 0
+
+    def swap(self, data, index1, index2):
+        temp = data[index1]
+        data[index1] = data[index2]
+        data[index2] = temp
+
+    def sort(self, data):
+        self.sortHelper(data, 0, len(data) - 1)
+        return data
+
+    def sortHelper(self, data, low, high):
+        if high <= low:
+            return
+        
+        pivotIndex = self.medianOfMedians(data, low, high)
+        pivotNewIndex = self.partition(data, low, high, pivotIndex)
+        
+        self.sortHelper(data, low, pivotNewIndex - 1)
+        self.sortHelper(data, pivotNewIndex + 1, high)
+
+    def partition(self, data, low, high, pivotIndex):
+        pivotValue = data[pivotIndex]
+        self.swap(data, pivotIndex, high)  # Move pivot to end
+        storeIndex = low
+
+        for i in range(low, high):
+            if data[i] < pivotValue:
+                self.swap(data, i, storeIndex)
+                storeIndex += 1
+
+        self.swap(data, storeIndex, high)  # Move pivot to its final place
+        return storeIndex
+
+    def medianOfMedians(self, data, low, high):
+        # For 5 or fewer elements just get median
+        if high - low < 5:
+            return self.partition5(data, low, high)
+        
+        # Otherwise move the medians of five-element subgroups to the first n/5 positions
+        for i in range(low, high + 1, 5):
+            subHigh = i + 4 if i + 4 <= high else high
+            median5 = self.partition5(data, i, subHigh)
+            self.swap(data, median5, low + int((i - low) / 5))
+
+        # Compute the median of the n/5 medians-of-five
+        mid = int((high - low) / 10) + low + 1
+        return self.medianOfMedians(data, low, low + int((high - low) / 5))
+
+    def partition5(self, data, low, high):
+        # Insertion sort
+        for i in range(low + 1, high + 1):
+            for j in range(i, low, -1):
+                if data[j - 1] > data[j]:
+                    self.swap(data, j - 1, j)
+                else:
+                    break
+        return (high + low) // 2  # Return median of sorted 5 or fewer elements
